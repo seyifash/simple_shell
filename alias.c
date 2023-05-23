a6d896a56082b4b65078e3f60764a99bc7b7ed64
karimbasegod <karimyac2@gmail.com>
2023-05-23 10:18:53 -0700

    commit

--- alias.c
+++ alias.c
@@ -1,118 +1,112 @@
 #include "shell.h"
+struct AliasData
+{
 char aliases[ALEN][VLEN];
 char values[ALEN][VLEN];
-int aliasCount = 0;
-
-/**
- * all-aliases - prints all alias
- *
- */
-
-void all_aliases()
+int aliasCount;
+};
+void all_aliases(const struct AliasData *data);
+int aliasfunc(struct AliasData *data, char **token,
+int status, char *filename);
+void setalias(struct AliasData *data, char **tokens);
+void aliasvalue(const struct AliasData *data, char *aliasName);
+void all_aliases(const struct AliasData *data)
 {
-	char output[1000];
-	int i;
-    
-	for (i = 0; i < aliasCount; i++)
-	{
-	_strcat(output, aliases[i]);
-	_strcat(output, "=");
-	_strcat(output, values[i]);
-	_strcat(output, "\n");
-	}
-	write(STDOUT_FILENO, output, strlen(output));
+char output[1000];
+int i;
+for (i = 0; i < aliasCount; i++)
+{
+_strcat(output, aliases[i]);
+_strcat(output, "=");
+_strcat(output, values[i]);
+_strcat(output, "\n");
+}
+write(STDOUT_FILENO, output, strlen(output));
 }
 /**
- * aliasfunc - checks for argument after the alias command
- * @token: the command entered
- * @status: unused
- * @filename: unused
- * Return: returns an 0on success
- */
-
+* aliasfunc - checks for argument after the alias command
+* @token: the command entered
+* @status: unused
+* @filename: unused
+* Return: returns 0 on success
+*/
 int aliasfunc(char **token, int status, char *filename)
 {
-	int i;
-	(void)status;
-	(void)filename;
-
-	if (token[0] == NULL)
-	{
-	all_aliases();
-	return (0);
-	}
-	else if (token[1] != NULL && _strchr(token[1], '='))
-	{
-        setalias(token);
-	return (0);
-	}
-	else
-	{
-	i = 1;
-
-	while (token[i] != NULL)
-	{
-	aliasvalue(token[i]);
-	i++;
-	return (0);
-	}
-	}
-
-	return (-1);
+int i;
+(void)status;
+(void)filename;
+if (token[0] == NULL)
+{
+all_aliases();
+return (0);
+}
+else if (token[1] != NULL && _strchr(token[1], '='))
+{
+setalias(token);
+return (0);
+}
+else
+{
+i = 1;
+while (token[i] != NULL)
+{
+aliasvalue(token[i]);
+i++;
+return (0);
+}
+}
+return (-1);
 }
 /**
- * setalias - sets the alais if thers a given value
- * @tokens: the command entered
- *
- */
+* setalias - sets the alias if there's a given value
+* @tokens: the command entered
+*/
 void setalias(char **tokens)
 {
-	int i;
-	char *name = _strtok(tokens[1], "=");
-	char *value = _strtok(NULL, "=");
-
-	if (name != NULL && value != NULL)
-	{
-	for (i = 0; i < aliasCount; i++)
-	{
-	if (_strcmp(aliases[i], name) == 0)
-	{	    
-	_strcpy(values[i], value);
-	return;
-	}
-	}
-
-	if (aliasCount < ALEN) {
-	_strcpy(aliases[aliasCount], name);
-	_strcpy(values[aliasCount], value);
-	aliasCount++;
-	}
-	}
+int i;
+char *name = _strtok(tokens[1], "=");
+char *value = _strtok(NULL, "=");
+if (name != NULL && value != NULL)
+{
+for (i = 0; i < aliasCount; i++)
+{
+if (_strcmp(aliases[i], name) == 0)
+{
+_strcpy(values[i], value);
+return;
+}
+}
+if (aliasCount < ALEN)
+{
+_strcpy(aliases[aliasCount], name);
+_strcpy(values[aliasCount], value);
+aliasCount++;
+}
+}
 }
 /**
- * aliasvalue - checks for the value assigned to a name
- * @alaisName: the name for the alias to be checked
- *
- */
-void aliasvalue(char *aliasName) 
+* aliasvalue - checks for the value assigned to a name
+* @aliasName: the name for the alias to be checked
+*/
+void aliasvalue(char *aliasName)
 {
-	char *aliasValue = NULL;
-	char output[ALEN + VLEN + 2];
-	int i;
-
-	for (i = 0; i < aliasCount; i++) {
-        if (_strcmp(aliases[i], aliasName) == 0) {
-            aliasValue = values[i];
-            break;
-	}
-	}
-    
-	if (aliasValue != NULL)
-	{
-	_strcpy(output, aliasName);	
-	_strcat(output, "=");
-	_strcat(output, aliasValue);
-	write(STDOUT_FILENO, output, strlen(output));
-	write(STDOUT_FILENO, "\n", 1);
-	}
+char *aliasValue = NULL;
+char output[ALEN + VLEN + 2];
+int i;
+for (i = 0; i < aliasCount; i++)
+{
+if (_strcmp(aliases[i], aliasName) == 0)
+{
+aliasValue = values[i];
+break;
+}
+}
+if (aliasValue != NULL)
+{
+_strcpy(output, aliasName);
+_strcat(output, "=");
+_strcat(output, aliasValue);
+write(STDOUT_FILENO, output, strlen(output));
+write(STDOUT_FILENO, "\n", 1);
+}
 }
--- string.c
+++ string.c
@@ -1,104 +1,95 @@
 #include "shell.h"
-
 /**
- * _strncpy - copies a string with n
- * @dest: copy to
- * @src: copy from
- * @n: number
- * Return: number of char to be pointed to
- */
-
+* _strncpy - copies a string with n
+* @dest: copy to
+* @src: copy from
+* @n: number
+* Return: number of char to be pointed to
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j = 0;
-
-	while (j < n && src[j] != '\0')
-        {
-	dest[j] = src[j];
-	j++;
-	}
-	while (j < n)
-	{
-	dest[j] = '\0';
-	j++;
-	}
-	return (dest);
+int j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
 }
-
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
 /**
- * _putchar - prints a character to stdout
- * @c: the character given
- * Return: 1 if success, else -1.
- */
-
+* _putchar - prints a character to stdout
+* @c: the character given
+* Return: 1 if success, else -1.
+*/
 int _putchar(int c)
 {
-	return (write(STDOUT_FILENO, &c, 1));
+return (write(STDOUT_FILENO, &c, 1));
 }
-
 /**
- * print - prints a string to stdout
- * @str: the given string to be printed out
- *
- *
- * Return: the number of bytes of @str printed, else -1
- */
+* print - prints a string to stdout
+* @str: the given string to be printed out
+*
+*
+* Return: the number of bytes of @str printed, else -1
+*/
 int print(char *str)
 {
-	int n_bytes = 0;
-
-	n_bytes = _strlen(str);
-	return (write(STDOUT_FILENO, str, n_bytes));
+int n_bytes = 0;
+n_bytes = _strlen(str);
+return (write(STDOUT_FILENO, str, n_bytes));
 }
-
 /**
- * _isalpha - checks if a character is a letter
- * @c: the given character
- *
- * Return: TRUE if true, else FALSE
- */
-
+* _isalpha - checks if a character is a letter
+* @c: the given character
+*
+* Return: TRUE if true, else FALSE
+*/
 int _isalpha(char c)
 {
-	if ((c >= 48) && (c <= 57))
-	return (FALSE);
-	return (TRUE);
+if ((c >= 48) && (c <= 57))
+return (FALSE);
+return (TRUE);
 }
-
 /**
  * _atoi - convert a string to an integer
- * @s:the string to be converted
+ * @s: the string to be converted
  *
  * Return: int
  */
 int _atoi(char *s)
 {
-	int len = 0;
-	int i = 0;
-	int flag = 0;
-	int d = 0, n = 0, digit;
-
-	for (len = 0; s[len] != '\0'; len++);
-
-	while (i < len && flag == 0)
-	{
-	if (s[i] == '-')
-	++d;
-	if (s[i] >= 48 && s[i] <= 57)
-	{
-	digit = s[i] - '0';
-	if (d % 2)
-	digit = -digit;
-	n = n * 10 + digit;
-	flag = 1;
-	if (s[i + 1] < 48 || s[i + 1] > 57)
-	break;
-	flag = 0;
-	}
-	i++;
-	}
-	if (flag == 0)
-	return (0);
-
-	return (n);
+int len = 0;
+int i = 0;
+int flag = 0;
+int d = 0, n = 0, digit;
+len = 0;
+while (s[len] != '\0');
+len++;
+{
+}
+while (i < len && flag == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= 48 && s[i] <= 57)
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+flag = 1;
+if (s[i + 1] < 48 || s[i + 1] > 57)
+break;
+flag = 0;
+}
+i++;
+}
+if (flag == 0)
+return (0);
+return (n);
 }
