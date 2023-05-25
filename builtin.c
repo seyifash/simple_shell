802e3e9bf3e8e9174894c011d6b6a2a7797e4ad4
karimbasegod <karimyac2@gmail.com>
2023-05-25 15:59:20 -0700

    commit

---
+++ builtin.c
@@ -0,0 +1,90 @@
+#include "shells.h"
+/**
+* is_builtin - checks if the command given is a built in function
+* @cmd: command entered
+* Return: the position of cmd in the builtin array
+*/
+builtin_t is_builtin(char *cmd)
+{
+int i;
+builtin_t builtincommands[] = {
+{"exit", exit_cmd},
+{"env", env_cmd},
+{"cd", cd_cmd},
+{NULL, NULL}
+};
+for (i = 0; builtincommands[i].cmd != NULL; i++)
+if (_strcmp(builtincommands[i].cmd, cmd) == 0)
+return (builtincommands[i]);
+return (builtincommands[i]);
+}
+/**
+* check_builtins - if command is a builtin command
+* @cmd: an array of command and its argument
+*
+* Return: the appropriate function to be executed, else NULL
+*/
+int (*check_builtins(char **cmd))(char **, int, char *)
+{
+builtin_t func = is_builtin(cmd[0]);
+if (func.cmd)
+return (func.function_ptr);
+return (NULL);
+}
+/**
+* env_cmd - builtin implementation of env command
+* @cmd: unused
+* @status: the status code
+* @filename: name of file
+*
+* Return: Always 0
+*/
+int env_cmd(char **cmd, int status, char *filename)
+{
+int i;
+(void)cmd;
+(void)status;
+(void) filename;
+for (i = 0; environ[i]; i++)
+{
+print(environ[i]);
+_putchar('\n');
+}
+return (0);
+}
+/**
+* exit_cmd - builtin implementation of the exit command
+* @cmd: an array of given commands and its argument
+* @status: the status code
+* @filename: name of file
+*
+* Return: exit with the status code given or previous execution
+* status code
+*/
+int exit_cmd(char **cmd, int status, char *filename)
+{
+int i = 0;
+if (!cmd[i])
+{
+freememory_pp(cmd);
+exit(status);
+}
+while (cmd[1][i])
+{
+if (_isalpha(cmd[1][i]) && cmd[1][i] != '-')
+{
+print(filename);
+print(": ");
+print(cmd[0]);
+print(": ");
+print("Illegal number: ");
+print(cmd[1]);
+_putchar('\n');
+return (1);
+}
+i++;
+}
+status = _atoi(cmd[1]);
+freememory_pp(cmd);
+exit(status);
+}
